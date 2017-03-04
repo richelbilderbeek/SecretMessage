8f977d6c6614fa79e3410411fb8402d8e974a7f7
richelbilderbeek <richel@richelbilderbeek.nl>
2017-03-04 01:04:29 +0100

    Compile and download less

--- qtsecretmessagemenudialog.cpp
+++ qtsecretmessagemenudialog.cpp
@@ -7,8 +7,6 @@
 #include "qtsecretmessageextractdialog.h"
 #include "qtsecretmessagemenudialog.h"
 #include "secretmessagemenudialog.h"
-#include "testtimer.h"
-#include "trace.h"
 #include "ui_qtsecretmessagemenudialog.h"
 #pragma GCC diagnostic pop
 
@@ -16,9 +14,6 @@ ribi::sema::QtMenuDialog::QtMenuDialog(QWidget *parent)
   : QtHideAndShowDialog(parent),
     ui(new Ui::QtSecretMessageMenuDialog)
 {
-  #ifndef NDEBUG
-  Test();
-  #endif
   ui->setupUi(this);
 }
 
@@ -56,15 +51,3 @@ void ribi::sema::QtMenuDialog::on_button_quit_clicked()
 {
   close();
 }
-
-#ifndef NDEBUG
-void ribi::sema::QtMenuDialog::Test() noexcept
-{
-  {
-    static bool is_tested{false};
-    if (is_tested) return;
-    is_tested = true;
-  }
-  const TestTimer test_timer(__func__,__FILE__,1.0);
-}
-#endif
--- qtsecretmessagemenudialog.h
+++ qtsecretmessagemenudialog.h
@@ -26,10 +26,6 @@ private slots:
   void on_button_quit_clicked();
   void on_button_extract_clicked();
 
-  #ifndef NDEBUG
-  static void Test() noexcept;
-  #endif
-
 private:
   Ui::QtSecretMessageMenuDialog *ui;
 };
